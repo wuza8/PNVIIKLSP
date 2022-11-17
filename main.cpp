bdb2057361c823f2c85ce8e8ff8e9675a6459d0e
AyBiCi <rymcymcym100@gmail.com>
2022-11-17 13:07:05 +0100

    ksdfjsklj

--- main.cpp
+++ main.cpp
@@ -1,85 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include "math/linearfunction.h"
+#include "math/vertex.h"
+#include "pnviiklsp.h"
 
 using namespace std;
 
-class Vertex {
-    double x, y;
-    public:
-    Vertex(double x, double y) {
-        this->x = x;
-        this->y = y;
-    }
-
-    double getX(){
-        return x;
-    }
-    double getY(){
-        return y;
-    }
-};
-
-class LinearFunction {
-    double a, b;
-
-public:
-    LinearFunction(double a, double b) {
-    this->a = a;
-    this->b = b;
-    }
-
-    double getA() {
-        return a;
-    }
-
-    double getB() {
-        return b;
-    }
-    //TODO testy
-    double countY(double x) {
-        return this->getA() * x + this->getB();
-    }
-
-    /*
-    Counts function intersection point with each other
-
-    Returns nullptr if intersection doesn't exist
-    */
-    //TODO coś nie tak, bo nie zwróci nigdy nulla, ale to poootem xD
-    //TODO testy
-    Vertex* countIntersection(LinearFunction* func) {
-        double x = (func->getB() - this->getB()) / (this->getA() - func->getA());
-        return new Vertex(x, this->countY(x));
-    }
-};
-
 //TODO testy
-void loadFunctions(vector<LinearFunction*>& functions, int count) {
-    for (int i = 1; i <= count; i++) {
-    double a, b;
-    cout << "Podaj funkcję nr. " << i << endl;
-    cout << "\t a="; cin >> a;
-    cout << "\t b="; cin >> b;
-    functions.push_back(new LinearFunction(a, b));
-}
-}
 
-vector<LinearFunction*> loadFunctionsFromCmdLine(int argc, char *argv[]) {
-    vector<LinearFunction*> functions;
-    cout<<argc<<endl;
-    if(argc % 2 != 1){
-        cout<<"Podaj parzystą ilość argumentów!"<<endl;
-        exit(1);
-    }
-    int nums = (argc-1) / 2;
-    for (int i = 1; i < argc; i+=2) {
-        double a, b;
-        a = stoi(argv[i]);
-        b = stoi(argv[i+1]);
-        functions.push_back(new LinearFunction(a, b));
-    }
-    return functions;
-}
 
 //TODO testy
 vector<Vertex*> countVertices(vector<LinearFunction*> & functions) {
@@ -88,7 +17,6 @@ vector<Vertex*> countVertices(vector<LinearFunction*> & functions) {
         for (int j = i + 1; j < functions.size(); j++)
         {
             Vertex* vertex = functions[i]->countIntersection(functions[j]);
-            cout<<vertex->getX()<<" "<<vertex->getY()<<endl;
             if (vertex != NULL) vertices.push_back(vertex);
         }
     return vertices;
@@ -107,17 +35,13 @@ void distinctVertices(vector<Vertex*> vertices) {
 }
 
 int main(int argc, char *argv[]) {
-    setlocale(LC_CTYPE, "Polish");
+    PNVIIKLSP program;
+    program.start(argc,argv);
+}
 
+void parseCommand(std::string commandData){
+    std::stringstream stream(commandData);
 
-    vector<LinearFunction*> functions;
-    int ile_funkcji = 4;
-    if(argc > 1)
-        functions = loadFunctionsFromCmdLine(argc,argv);
-    else
-        loadFunctions(functions, ile_funkcji);
-    
-    vector<Vertex*> vertices = countVertices(functions);
-    distinctVertices(vertices);
-    displayVerticesInfo(vertices);
+    string command;
+    stream>>command;
 }
